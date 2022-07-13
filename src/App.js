00da70635bb05fba4489cf8d14d7c498fc94c62d
Potok-7 <karabin.m@yandex.ru>
2022-07-13 22:35:28 +0700

    [main] tmp changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Checkbox from './widgets/checkbox/index.tsx';
 
 function App() {
   return (
     <div className="App">
-
+      <Checkbox/>
     </div>
   );
 }
---
+++ src/widgets/checkbox/index.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from "react";
+
+const Checkbox: FC = () => {
+  return (
+    <div/>
+  );
+}
+
+
+export default Checkbox;
